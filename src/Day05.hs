b697245be01da9a29672a976e76192272730ca0f
Tom Jemmett <thomas.jemmett@nhs.net>
2025-01-10 11:23:15 +0000

    adds solution for day 5

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,11 +1,84 @@
-module Day05 (day05, day05TestInput) where
+-- module Day05 (day05, day05TestInput) where
+module Day05 where
 
 import Common
-import Text.Parsec qualified as P
-import Text.Parsec.String (Parser)
+import Control.Monad.State
+import Data.HashMap.Strict qualified as M
+import Data.HashSet qualified as S
+import Data.List (transpose)
+import Data.List.Split (splitOn)
+import Data.Sequence (Seq ((:<|)))
+import Data.Sequence qualified as SQ
+import Data.Vector qualified as V
+
+type Dancers = V.Vector (SQ.Seq Int)
+
+type Shouts = [String]
 
 day05TestInput :: String
-day05TestInput = ""
+day05TestInput =
+  "2 3 4 5\n\
+  \3 4 5 2\n\
+  \4 5 2 3\n\
+  \5 2 3 4\n\
+  \\n\
+  \2 3 4 5\n\
+  \6 7 8 9\n\
+  \\n\
+  \2 3 4 5\n\
+  \6 7 8 9"
 
 day05 :: ECSolution
-day05 input = []
+day05 input = [part1 i1, part2 i2, part3 i3]
+  where
+    [i1, i2, i3] = parseInput input
+
+part1 :: Dancers -> String
+part1 = shout . snd . (!! 10) . iterate (execState dance) . (0,)
+
+part2 :: Dancers -> String
+part2 i = show $ go 0 M.empty states
+  where
+    states = map (shout . snd) $ iterate (execState dance) (0, i)
+    go r m (x : xs) = if m' M.! x == 2024 then r * read x else go (succ r) m' xs
+      where
+        m' = M.insertWith (+) x 1 m
+
+part3 :: Dancers -> String
+part3 = evalState (go S.empty "") . (0,)
+  where
+    go :: S.HashSet String -> String -> State (Int, Dancers) String
+    go s m = do
+      i <- get
+      if show i `S.member` s
+        then pure m
+        else do
+          dance
+          i' <- get
+          let s' = S.insert (show i) s
+          go s' (max m $ shout (snd i'))
+
+parseInput :: String -> [Dancers]
+parseInput = map fn . splitOn "\n\n"
+  where
+    fn = V.fromList . map SQ.fromList . transpose . map (map read . words) . lines
+
+dance :: State (Int, Dancers) ()
+dance = do
+  (i, v) <- get
+  let (x :<| s) = v V.! i
+      i' = succ i `mod` V.length v
+  put (i', v V.// [(i, s)])
+  shuffle x
+
+shuffle :: Int -> State (Int, Dancers) ()
+shuffle c = do
+  (i, v) <- get
+  let s = v V.! i
+      l = SQ.length s
+      n = (c - 1) `mod` (2 * l)
+      n' = if n >= l then (2 * l) - n else n
+  put (i, v V.// [(i, SQ.insertAt n' c s)])
+
+shout :: Dancers -> String
+shout = concatMap (\(x :<| _) -> show x)
--- test/Day05Spec.hs
+++ test/Day05Spec.hs
@@ -5,7 +5,7 @@ import SpecHelper
 spec :: Spec
 spec = describe "Day 5" $ do
   it "Sample" $ do
-    day05 day05TestInput `shouldBe` []
+    day05 day05TestInput `shouldBe` ["2323", "50877075", "6584"]
 
   it "Actual" $ do
     withFile
@@ -13,5 +13,5 @@ spec = describe "Day 5" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day05 actualInput `shouldBe` []
+          day05 actualInput `shouldBe` ["4245", "20502741815899", "8371100810001000"]
       )
