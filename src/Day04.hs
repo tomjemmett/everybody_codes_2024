310a6c5dea1febbc348fcb3cd8946cc8b64975ae
Tom Jemmett <thomas.jemmett@nhs.net>
2025-01-10 11:21:07 +0000

    adds day 4 solution

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,11 +1,30 @@
 module Day04 (day04, day04TestInput) where
 
 import Common
-import Text.Parsec qualified as P
-import Text.Parsec.String (Parser)
+import Data.List.Split (splitOn)
 
 day04TestInput :: String
-day04TestInput = ""
+day04TestInput =
+  "3\n4\n7\n8\n\n\
+  \3\n4\n7\n8\n\n\
+  \2\n4\n5\n6\n8"
 
 day04 :: ECSolution
-day04 input = []
+day04 input = show <$> [part1 p1, part2 p2, part3 p3]
+  where
+    [p1, p2, p3] = parseInput input
+
+parseInput :: String -> [[Int]]
+parseInput = map (map read . lines) . splitOn "\n\n"
+
+solve :: Int -> [Int] -> Int
+solve m = sum . map (abs . subtract m)
+
+part1 :: [Int] -> Int
+part1 xs = solve (minimum xs) xs
+
+part2 :: [Int] -> Int
+part2 = part1
+
+part3 :: [Int] -> Int
+part3 xs = minimum $ map (`solve` xs) xs
--- test/Day04Spec.hs
+++ test/Day04Spec.hs
@@ -5,7 +5,7 @@ import SpecHelper
 spec :: Spec
 spec = describe "Day 4" $ do
   it "Sample" $ do
-    day04 day04TestInput `shouldBe` []
+    day04 day04TestInput `shouldBe` ["10", "10", "8"]
 
   it "Actual" $ do
     withFile
@@ -13,5 +13,5 @@ spec = describe "Day 4" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day04 actualInput `shouldBe` []
+          day04 actualInput `shouldBe` ["67", "898681", "124770256"]
       )
