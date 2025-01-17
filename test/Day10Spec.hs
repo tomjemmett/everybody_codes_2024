6a1e268da608ac37881fb35a974bf4644c11ec12
Tom Jemmett <thomas.jemmett@nhs.net>
2025-01-17 22:32:39 +0000

    adds solution for day 10

--- test/Day10Spec.hs
+++ test/Day10Spec.hs
@@ -5,7 +5,7 @@ import SpecHelper
 spec :: Spec
 spec = describe "Day 10" $ do
   it "Sample" $ do
-    day10 day10TestInput `shouldBe` []
+    day10 day10TestInput `shouldBe` ["PTBVRCZHFLJWGMNS", "1851", "3889"]
 
   it "Actual" $ do
     withFile
@@ -13,5 +13,5 @@ spec = describe "Day 10" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day10 actualInput `shouldBe` []
+          day10 actualInput `shouldBe` ["XHRFNBMZTSVQWJLP", "196798", "208713"]
       )
